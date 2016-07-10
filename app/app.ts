8c6c92293e2327ef87e9dd0ef56dd02d4a90815b
Marco Heumann (mheumann) <marco.heumann@posteo.de>
2016-07-10 19:33:16 +0200

    added corova-plugin-geolocation

--- app/app.ts
+++ app/app.ts
@@ -11,12 +11,12 @@ export class MyApp {
     private rootPage: any;
 
     constructor(private platform: Platform) {
-        this.rootPage = MapPage;
-
         platform.ready().then(() => {
             // Okay, so the platform is ready and our plugins are available.
             // Here you can do any higher level native things you might need.
             StatusBar.styleDefault();
+            
+            this.rootPage = MapPage;
         });
     }
 }
--- app/pages/map-page/map-page.ts
+++ app/pages/map-page/map-page.ts
@@ -1,26 +1,50 @@
 import {Page} from 'ionic-angular';
 import * as L from "leaflet";
-import {Map} from 'leaflet';
+import {Map, LatLng, Marker} from 'leaflet';
 
 @Page({
     templateUrl: 'build/pages/map-page/map-page.html'
 })
 export class MapPage {
     private map: Map;
-    
+    private latLng: LatLng;
+    private watchId: number;
+    private posMarker: Marker;
+
     constructor() { }
 
     onPageWillEnter() {
-        this.map = L.map('map');
+        let options = { timeout: 10000, enableHighAccuracy: true };
+
+        this.map = L.map('map', {
+            zoom: 13
+        });
 
         L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
             maxZoom: 18
         }).addTo(this.map);
+
+        navigator.geolocation.watchPosition((curPos) => this.showInitialLocation(curPos), (error) => {
+            console.log("Initial position locating failed");
+            console.log(error);
+        }, options);
+
+        this.watchId = navigator.geolocation.watchPosition((curPos) => {
+            console.log("View is changing");
+//            this.latLng = new L.LatLng(curPos.coords.latitude, curPos.coords.longitude);
+//            this.posMarker.setLatLng(this.latLng);
+        }, (error) => {
+            console.log(error);
+        }, options);
     }
-    
-    onPageDidEnter() {
-        L.marker([51.505, -0.09]).addTo(this.map);
-        this.map.setView([51.505, -0.09], 13);
+
+    showInitialLocation(curPos: Position) {
+        this.latLng = new L.LatLng(curPos.coords.latitude, curPos.coords.longitude);
+        
+        this.posMarker = L.marker(this.latLng);        
+        this.posMarker.addTo(this.map);
+        
+        this.map.setView(this.latLng);
     }
 }
