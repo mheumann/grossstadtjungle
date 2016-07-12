8f3d95f585e92b29864469894c5888f5519b483a
mheumann <marco.heumann@poste.de>
2016-07-12 23:07:50 +0200

    upgrading to beta 10 and working MapService

--- app/pages/map-page/map-page.ts
+++ app/pages/map-page/map-page.ts
@@ -1,55 +1,22 @@
 import {Page, Nav} from 'ionic-angular';
-import * as L from "leaflet";
+import * as L from 'leaflet';
 import {Map, LatLng, Marker, Circle} from 'leaflet';
+import {MapService} from '../../services/map-service';
 
 @Page({
-    templateUrl: 'build/pages/map-page/map-page.html'
+    templateUrl: 'build/pages/map-page/map-page.html',
+    providers: [MapService]
 })
 export class MapPage {
-    private map: Map;
-    private latLng: LatLng;
-    private watchId: number;
-    private posMarker: Marker;
-    private posCircle: Circle;
-
-    constructor(private nav: Nav) { }
+    constructor(private nav: Nav, private mapService: MapService) { }
 
     onPageWillEnter() {
-        let options = { timeout: 10000, enableHighAccuracy: true };
-
-        this.map = L.map('map', {
-            zoom: 13
+        let map = L.map('map', {
+            zoom: 13,
         });
-
-        L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
-            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
-            maxZoom: 19
-        }).addTo(this.map);
-
-        navigator.geolocation.getCurrentPosition((curPos) => this.showInitialLocation(curPos), (error) => {
-            console.log("Initial position locating failed");
-            console.log(error);
-        }, options);
-
-        this.watchId = navigator.geolocation.watchPosition((curPos) => {
-            this.latLng = new L.LatLng(curPos.coords.latitude, curPos.coords.longitude);
-            this.posMarker.setLatLng(this.latLng);
-            this.posCircle.setLatLng(this.latLng);
-            this.posCircle.setRadius(curPos.coords.accuracy / 2);
-        }, (error) => {
-            console.log(error);
-        }, options);
-    }
-
-    showInitialLocation(curPos: Position) {
-        this.latLng = new L.LatLng(curPos.coords.latitude, curPos.coords.longitude);
-
-        this.posMarker = L.marker(this.latLng);
-        this.posMarker.addTo(this.map);
         
-        this.posCircle = L.circle(this.latLng, curPos.coords.accuracy / 2);
-        this.posCircle.addTo(this.map);
-
-        this.map.setView(this.latLng);
+        this.mapService.map = map;
+        
+        this.mapService.startMapService();
     }
 }
--- app/services/map-service.ts
+++ app/services/map-service.ts
@@ -1,10 +1,49 @@
 import {Injectable} from '@angular/core';
-import {Map} from 'leaflet';
 import * as L from 'leaflet';
+import {Map, Marker, LatLng, Circle} from 'leaflet';
 
 @Injectable()
 export class MapService {
     map: Map;
-    
+    private latLng: LatLng;
+    private watchId: number;
+    private posCircle: Circle;
+    private posMarker: Marker;
+
     construct() { }
+
+    startMapService() {
+        let options = { timeout: 10000, enableHighAccuracy: true };
+
+        L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
+            maxZoom: 19
+        }).addTo(this.map);
+
+        navigator.geolocation.getCurrentPosition((curPos) => this.showInitialLocation(curPos), (error) => {
+            console.log("Initial position locating failed");
+            console.log(error);
+        }, options);
+
+        this.watchId = navigator.geolocation.watchPosition((position) => this.showInitialLocation(position), (error) => {
+            console.log("Watch position failed");
+            console.log(error);
+        }, options);
+    }
+
+    private showInitialLocation(curPos: Position) {
+        this.latLng = new L.LatLng(curPos.coords.latitude, curPos.coords.longitude);
+
+        this.posMarker = L.marker(this.latLng);
+        this.posMarker.addTo(this.map);
+
+        this.posCircle = L.circle(this.latLng, curPos.coords.accuracy / 2);
+        this.posCircle.addTo(this.map);
+
+        this.map.setView(this.latLng);
+    }
+
+    private setPosition() {
+
+    }
 }
